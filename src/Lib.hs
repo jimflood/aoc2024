d7d39224bf068b267afbb1857f29caca6629cd75
Jim Flood <jflood@rvohealth.com>
2025-01-07 17:26:40 -0800

    added last changes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,7 @@ module Lib
       BoundedGrid,
       Grid,
       parseGrid,
+      gridFind,
       drawGrid,
       chunk,
       opchunk -- order preserved
@@ -22,6 +23,9 @@ type BoundedGrid = ((Int, Int), Grid)
 
 parseGrid :: [String] -> BoundedGrid
 parseGrid css = ((length (head css), length css) , Map.fromList [((x, y), c) | (y, cs) <- zip [0..] css, (x, c) <- zip [0..] cs])
+
+gridFind :: Char -> Grid -> Coordinate
+gridFind c g = head $ Map.keys $ Map.filter (==c) g
 
 drawGrid :: BoundedGrid -> String
 drawGrid ((mx, my), m) = unlines [draw' y | y <- [0..my - 1]]
@@ -45,3 +49,11 @@ opchunk = opchunk' [] []
             | otherwise = opchunk' acc (x : t) xs
         opchunk' acc t [] = reverse (reverse t : acc)
         opchunk _ _ _ = error "bad input"
+
+--import Data.Char (chr, ord)
+--drawGraph :: DirectedGraph -> String
+--drawGraph dg = unlines $ ["digraph g { "] ++ concat [[dotnode a, dotnode b, dotedge a b] | a <- Map.keys dg, b <- dg Map.! a] ++ ["}"]
+--    where
+--        dotnode a = identifier a ++ " [label=\"" ++ show a ++ "\"];"
+--        dotedge a b = identifier a ++ " -> " ++ identifier b ++ ";"
+--        identifier ((x, y), d) = "_" ++ show x ++ "_" ++ show y ++ "_" ++ [chr (ord d `mod` 5 + 65)]
